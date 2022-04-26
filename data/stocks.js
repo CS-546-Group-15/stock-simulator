0405a5752e6516030b9f4d300b3c15f51a0f81a4
Enrique Valentino <enriquefvalentino@gmail.com>
2022-04-26 17:11:56 -0400

    added sellStock, not tested yet

--- data/stocks.js
+++ data/stocks.js
@@ -44,7 +44,7 @@ async function getStockBySymbol(symbol) {
 
 
 // called when a user wants to buy a stock
-async function buyStock(userId, symbol, shares) {
+async function buyStock(userId, symbol, shares) { // TODO: STILL NEED TO DEAL WITH BOUNDS i.e. user doesn't have enough money
     // validate inputs
     validation.checkId(userId);
     validation.checkSymbol(symbol);
@@ -122,15 +122,68 @@ async function buyStock(userId, symbol, shares) {
     return { purchased: true };
 }
 // called when a user wants to sell a stock
-async function sellStock(userId, symbol, shares) {
-    // TODO: validate inputs
-    const userCollection = await users();
+async function sellStock(userId, symbol, shares) { // TODO: STILL NEED TO DEAL WITH BOUNDS i.e. user doesn't have enough shares to sell
+    // validate inputs
+    validation.checkId(userId);
+    validation.checkSymbol(symbol);
+    validation.checkShares(shares);
 
-    // TODO: needs to be rewritten
+    // format inputs
+    userId = userId.trim();
+    symbol = symbol.trim();
+
+    let date_time = new Date().toUTCString(); // date and time of purchase
+
+    // get purchasing user
+    const userCollection = await users();
+    const user = await userCollection.findOne({ _id: ObjectId(userId) });
+    if (!user) throw `User doesn't exist with id ${userId}`;
+
+    // get stock from API call
+    stockApiData = await getStockBySymbol(symbol);
+
+    // check if symbol could be found
+    if(stockApiData.length < 1) throw `Could not find stock with symbol ${symbol}`;
+    // api call returns an array, to get desired stock, get first element of that array
+    stockData = stockApiData[0];
+
+    // set price sold to last sale price
+    let price_sold = stockData.lastSalePrice;
+    let totalCost = price_sold * shares; // calculate for total cost
+
+    // check if user owns the stock being purchased
+    owned = user.stocks.filter(stock => stock.symbol === symbol);
+    if(owned.length < 1) { // case: user does not own the stock being sold, cannot sell stock
+        throw 'user does not own this stock';
+    } else { // user owns the stock being purchased
+        stock = owned[0]; // get owned stock info
+        avgPrice = ((stock.weighted_average_price * stock.num_shares) - (totalCost)) / (stock.num_shares - shares); // calculate weighted average price from previous average and new price
+
+        const updateInfo = await userCollection.updateOne( // update the owned stock subdocument
+            { 
+                _id: ObjectId(userId),
+                'stocks.symbol': symbol
+            },
+            {
+                $inc: { 
+                    cash: totalCost, // increment amount of cash
+                    'stocks.$.num_shares': -shares, // decrement number of shares
+                    'stocks.$.total_cost': -totalCost // decrement total cost
+                },
+                $set: {
+                    'stocks.$.weighted_average_price': avgPrice // set average price to recalculated weighted average price
+                }
+            }
+        );
+        // check for errors updating
+        if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw "Could not sell stock";
+    }
+    // might change this return type later, for now it's just a confirmation
+    return { sold: true };
 }
 
 module.exports = {
     getStockBySymbol,  
     buyStock,
-    sellStock,
+    sellStock
 }
