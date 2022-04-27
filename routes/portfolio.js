ed22ae629931549d5848973c819d0365fd6d175e
Andrew Narvaez <anarvaez12@verizon.net>
2022-04-27 12:20:50 -0400

    Porfolio - Get account assets

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data/');
 const users = data.users;
+const stocks = data.stocks;
 const { ObjectId } = require("mongodb");
 
 //show discussion page
 router.get('/', async (req, res) => {
     if(req.session.user) {
+        let userId = req.session.user.userId;
         let user = await users.getUser(req.session.user.username);
-        res.render('display/portfolio', {user: user, authenticated: true});
+        const userVal = await stocks.getAccVal(userId.toString());
+        console.log(userVal);
+        res.render('display/portfolio', {user: user, userVal: userVal, authenticated: true});
     } else {
         res.redirect('/login');
     }
