014a62be4687dba92872ab36a818ccf07b80beb7
Ryan Chin <ryanchin26@gmail.com>
2022-04-27 11:56:39 -0400

    Formulated discussions, worked on newPosts, more to come
    posts.js in routes will soon be deleted and replaced by discussion.js

--- routes/posts.js
+++ routes/posts.js
@@ -4,15 +4,39 @@ const router = express.Router();
 const validation = require('../validation');
 const postData = data.posts;
 
-//  View the discussion board
+//show discussion page
 router.get('/', async (req, res) => {
-  const postList = await postData.getAllPosts();
-  res.render('posts/index', {posts: postList});
+  console.log("landed");
+  if(req.session.user) {
+      // const postList = await postData.getAllPosts();
+      res.render('posts/discussion', {});
+  } else {
+      res.render('display/login', {});
+  }
+  return;
+});
+
+//  View the discussion board
+// router.get('/', async (req, res) => {
+//   const postList = await postData.getAllPosts();
+//   res.render('posts/discussion', {posts: postList});
+// });
+
+//show discussion page
+router.get('/newPost', async (req, res) => {
+  console.log("here");
+  if(req.session.user) {
+      res.render('posts/newPost', {});
+  } else {
+      res.render('display/login', {});
+  }
+  return;
 });
 
 //  Get a specific blog post by id
 router.get('/:id', async (req, res) => {
-    try {
+  console.log("id");
+  try {
       const post = await postData.getPostById(req.params.id);
       res.render('posts/single', {post: post});
     } catch (e) {
@@ -22,17 +46,21 @@ router.get('/:id', async (req, res) => {
 
 //  Get a blogs that contain tags given
 router.get('/tag/:tag', async (req, res) => {
-    const postList = await postData.getPostsByTag(req.params.tag);
-    res.render('posts/index', {posts: postList});
+  console.log("tag");
+  const postList = await postData.getPostsByTag(req.params.tag);
+    res.render('posts/posts', {posts: postList});
 });
 
 //  Post your blog to the discussion board
 router.post('/', async (req, res) => {
-    let { userID, title, info, tags } = req.body;
+  console.log("posting!");
+  //  we need to get the userId param
+    userId = req.session.userId;
+    let { title, tags, info } = req.body;
     try {
-        validation.checkCreatePost(userID, title, info, tags);
+        validation.checkCreatePost(userId, title, info, tags);
     } catch(e) {
-        res.render('/', { error: e,});
+        res.render('posts/newPost', { error: e,});
         return;
     }
   
@@ -40,13 +68,14 @@ router.post('/', async (req, res) => {
       const newPost = await createPost.addPost(userId, title, info, tags);
       res.redirect(`/${newPost._id}`);
     } catch (e) {
-      res.status(500).json({error: e});
+      res.status(500).render('posts/newPost', {error: e});
     }
 });
 
 //  Update/modify an existing post
 router.put('/:id', async (req, res) => {
-    let { userID, title, info, tags } = req.body;
+  console.log("idd");
+  let { userId, title, info, tags } = req.body;
     try {
       await postData.getPostById(req.params.id);
     } catch (e) {
@@ -63,6 +92,7 @@ router.put('/:id', async (req, res) => {
 
 //  Delete a post from the user and discussion board
 router.delete('/:id', async (req, res) => {
+  console.log("delete");
   try {
     await postData.getPostById(req.params.id);
   } catch (e) {
